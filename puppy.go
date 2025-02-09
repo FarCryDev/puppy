e185e02f3f76642154042cecc3502a164eed3ff4
Oleksandr Mykhailov <oleksandrmykhailov@Oleksandrs-MacBook-Air.local>
2025-02-09 14:34:54 +0200

    add new function

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/GoesToEleven/dog"
+
 func Bark() string {
 	return "woof"
 }
 
 func Barks() string {
 	return "Woof, woof, woof"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp("I am too old for this")
 }
